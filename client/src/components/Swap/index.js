e41b5e82ddbad43ad157902a0216e2fb95bead76
nefrob <nefrob.skyguy@gmail.com>
2021-10-01 21:24:25 -0600

    Swap full functionality implementation

--- client/src/components/Swap/index.js
+++ client/src/components/Swap/index.js
@@ -1,62 +1,395 @@
-import React from "react";
-import { Button, Card, Form } from "react-bootstrap";
+import React, { useContext, useState, useEffect } from "react";
+import { Alert, Button, Card, Form } from "react-bootstrap";
+
+import { AppContext } from "../App";
+
+import { toWei, fromWei } from "../../utils/utils";
 
 const Swap = () => {
+    const [state, dispatch] = useContext(AppContext);
+
+    const [ethPriceUSD, setEthPriceUSD] = useState(0);
+    const [chosenFrom, setChosenFrom] = useState("choose");
+    const [amountFrom, setAmountFrom] = useState(0);
+    const [chosenTo, setChosenTo] = useState("choose");
+    const [amountTo, setAmountTo] = useState(0);
+    const [pricesUSD, setPricesUSD] = useState({
+        from: 0,
+        to: 0,
+    });
+    const [exchangeRate, setExchangeRate] = useState(0);
+    const [inProgress, setInProgress] = useState(false);
+
+    const tokenOptions = Object.keys(state.tokens).map((symbol, index) => {
+        return (
+            <option key={index} value={symbol}>
+                {symbol}
+            </option>
+        );
+    });
+
+    useEffect(() => {
+        const getEthPriceUSD = async () => {
+            const url = `https://cex.io/api/last_price/ETH/USD`;
+            const res = await fetch(url);
+            const json = await res.json();
+            setEthPriceUSD(json.lprice);
+        };
+
+        getEthPriceUSD();
+    }, []);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        setInProgress(true);
+
+        try {
+            if (chosenFrom === "ETH") {
+                await state.exchanges[chosenTo].methods
+                    .ethToTokenExchange(toWei(amountTo))
+                    .send({
+                        value: toWei(amountFrom),
+                        from: state.account.address,
+                    });
+            } else if (chosenTo === "ETH") {
+                console.log(amountTo);
+
+                await state.tokens[chosenFrom].contract.methods
+                    .approve(
+                        state.exchanges[chosenFrom]._address,
+                        toWei(amountFrom)
+                    )
+                    .send({ from: state.account.address });
+
+                await state.exchanges[chosenFrom].methods
+                    .tokenToEthExchange(toWei(amountFrom), toWei(amountTo))
+                    .send({
+                        from: state.account.address,
+                    });
+            } else {
+                await state.tokens[chosenFrom].contract.methods
+                    .approve(
+                        state.exchanges[chosenFrom]._address,
+                        toWei(amountFrom)
+                    )
+                    .send({ from: state.account.address });
+
+                await state.exchanges[chosenFrom].methods
+                    .tokenToTokenExchange(
+                        toWei(amountFrom),
+                        toWei(amountTo),
+                        state.tokens[chosenTo].contract._address
+                    )
+                    .send({
+                        from: state.account.address,
+                    });
+            }
+
+            await updateBalances(); // fixme: need to do this?
+        } catch (error) {
+            console.log(error);
+        }
+
+        setAmountFrom(0);
+        setAmountTo(0);
+        setInProgress(false);
+    };
+
+    const updateBalances = async () => {
+        const ethBalance = await state.web3.eth.getBalance(
+            state.account.address
+        );
+        dispatch({
+            type: "UPDATE_ETH_BALANCE",
+            payload: fromWei(ethBalance),
+        });
+
+        if (chosenFrom !== "ETH") {
+            const tokenBalance = await state.tokens[chosenFrom].contract.methods
+                .balanceOf(state.account.address)
+                .call();
+            dispatch({
+                type: "UPDATE_TOKEN_BALANCE",
+                payload: {
+                    symbol: chosenFrom,
+                    balance: fromWei(tokenBalance),
+                },
+            });
+        }
+
+        if (chosenTo !== "ETH") {
+            const tokenBalance = await state.tokens[chosenTo].contract.methods
+                .balanceOf(state.account.address)
+                .call();
+            dispatch({
+                type: "UPDATE_TOKEN_BALANCE",
+                payload: {
+                    symbol: chosenTo,
+                    balance: fromWei(tokenBalance),
+                },
+            });
+        }
+    };
+
+    const handleSelectionChange = async (e) => {
+        const { name, value } = e.target;
+
+        setInProgress(true);
+
+        if (name === "chosenFrom") {
+            await selectionChangeHelper(
+                value,
+                chosenFrom,
+                setChosenFrom,
+                "from",
+                chosenTo,
+                setChosenTo,
+                "to"
+            );
+        } else {
+            await selectionChangeHelper(
+                value,
+                chosenTo,
+                setChosenTo,
+                "to",
+                chosenFrom,
+                setChosenFrom,
+                "from"
+            );
+        }
+
+        setInProgress(false);
+    };
+
+    const selectionChangeHelper = async (
+        newValue,
+        chooseA,
+        setChooseA,
+        keyA,
+        chooseB,
+        setChooseB,
+        keyB
+    ) => {
+        if (newValue === chooseA) {
+            return;
+        }
+
+        setChooseA(newValue);
+        setAmountFrom(0);
+        setAmountTo(0);
+
+        let newChooseB = chooseB;
+        if (newValue === chooseB) {
+            setChooseB("choose");
+            newChooseB = "choose";
+            setExchangeRate(0);
+            setPricesUSD({ ...pricesUSD, [keyB]: 0 });
+        }
+
+        if (newValue === "ETH") {
+            setPricesUSD({
+                ...pricesUSD,
+                [keyA]: ethPriceUSD,
+            });
+        } else if (newValue !== "choose") {
+            const poolSize = fromWei(
+                await state.exchanges[newValue].methods.totalSupply().call()
+            );
+
+            if (poolSize === "0") {
+                setChooseA("choose");
+                setExchangeRate(0);
+                setPricesUSD({ ...pricesUSD, [keyA]: 0 });
+                return;
+            }
+
+            try {
+                if (keyA === "to") {
+                    const ethToToken = fromWei(
+                        await state.exchanges[newValue].methods
+                            .getEthToTokenExchangeRate(toWei(1))
+                            .call()
+                    );
+
+                    setPricesUSD({
+                        ...pricesUSD,
+                        [keyA]: ethPriceUSD / ethToToken,
+                    });
+                } else {
+                    const ethToToken = fromWei(
+                        await state.exchanges[newValue].methods
+                            .getTokenToEthExchangeRate(toWei(1))
+                            .call()
+                    );
+
+                    setPricesUSD({
+                        ...pricesUSD,
+                        [keyA]: ethPriceUSD * ethToToken,
+                    });
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        if (newChooseB !== "choose") {
+            if (keyA === "from") {
+                setExchangeRate(await getExchangeRate(newValue, newChooseB));
+            } else {
+                setExchangeRate(await getExchangeRate(newChooseB, newValue));
+            }
+        }
+    };
+
+    const getExchangeRate = async (from, to) => {
+        let rate = 0;
+
+        try {
+            if (from === "ETH" && to !== "choose") {
+                rate = fromWei(
+                    await state.exchanges[to].methods
+                        .getEthToTokenExchangeRate(toWei(1))
+                        .call()
+                );
+            } else if (from !== "choose" && to === "ETH") {
+                rate = fromWei(
+                    await state.exchanges[from].methods
+                        .getTokenToEthExchangeRate(toWei(1))
+                        .call()
+                );
+            } else if (from !== "choose" && to !== "choose") {
+                const tokenToEth = await state.exchanges[from].methods
+                    .getTokenToEthExchangeRate(toWei(1))
+                    .call();
+                rate = fromWei(
+                    await state.exchanges[to].methods
+                        .getEthToTokenExchangeRate(tokenToEth)
+                        .call()
+                );
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
+        return rate;
+    };
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+
+        if (name === "amountFrom") {
+            setAmountFrom(value);
+
+            // fixme: lost value by truncating to 10 decimals
+            // should do all math with 18 fixed decimal places
+            if (exchangeRate !== 0) {
+                setAmountTo(parseFloat(value * exchangeRate).toFixed(6));
+            }
+        } else if (name === "amountTo") {
+            setAmountTo(value);
+
+            if (exchangeRate !== 0) {
+                setAmountFrom(parseFloat(value / exchangeRate).toFixed(6));
+            }
+        }
+    };
+
+    if (state.account.address === "0x0") {
+        return (
+            <Alert variant="warning">
+                You must connect a wallet before using this feature.
+            </Alert>
+        );
+    }
+
     return (
         <div className="Swap">
             <Card>
                 <Card.Header>Swap</Card.Header>
                 <Card.Body>
-                    <Card.Title>Exchange ETH/Tokens:</Card.Title>
+                    <Card.Title>
+                        Exchange ETH/Tokens or Tokens/Tokens:
+                    </Card.Title>
                     <Form>
-                        <Form.Group className="mb-2">
-                            <Form.Label>From:</Form.Label>
-                            <Form.Select>
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </Form.Select>
-                        </Form.Group>
+                        <Form.Label>From:</Form.Label>
+                        <Form.Select
+                            className="mb-2"
+                            name="chosenFrom"
+                            value={chosenFrom}
+                            onChange={handleSelectionChange}
+                            disabled={inProgress}
+                        >
+                            <option value="choose" hidden>
+                                Select swap from
+                            </option>
+                            <option value="ETH">ETH</option>
+                            {tokenOptions}
+                        </Form.Select>
                         <Form.Group className="mb-2">
                             <Form.Control
                                 className="number-form"
+                                name="amountFrom"
                                 type="number"
-                                placeholder="0.00"
-                                rows={3}
+                                placeholder="0"
+                                value={amountFrom}
+                                disabled={inProgress || chosenFrom === "choose"}
+                                onChange={handleInputChange}
                             />
                             <Form.Text className="text-muted">
-                                ~1 ETH = X USD
+                                ~$<samp>{pricesUSD.from * amountFrom}</samp>
                             </Form.Text>
                         </Form.Group>
-                        <Form.Group className="mb-2">
-                            <Form.Label>To:</Form.Label>
-                            <Form.Select>
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </Form.Select>
-                        </Form.Group>
+                        <Form.Label>To:</Form.Label>
+                        <Form.Select
+                            className="mb-2"
+                            name="chosenTo"
+                            value={chosenTo}
+                            onChange={handleSelectionChange}
+                            disabled={inProgress}
+                        >
+                            <option value="choose" hidden>
+                                Select swap to
+                            </option>
+                            <option value="ETH">ETH</option>
+                            {tokenOptions}
+                        </Form.Select>
                         <Form.Group className="mb-2">
                             <Form.Control
                                 className="number-form"
+                                name="amountTo"
                                 type="number"
-                                placeholder="0.00"
+                                placeholder="0"
+                                value={amountTo}
+                                disabled={inProgress || chosenTo === "choose"}
+                                onChange={handleInputChange}
                             />
                             <Form.Text className="text-muted">
-                                ~1 Token = X USD
+                                ~$<samp>{pricesUSD.to * amountTo}</samp>
                             </Form.Text>
                         </Form.Group>
                     </Form>
                     <Button
+                        className="mb-2"
                         variant="primary"
                         type="submit"
-                        onClick={() => {
-                            console.log("Swap");
-                        }}
-                        disabled={false}
+                        onClick={handleSubmit}
+                        disabled={
+                            inProgress ||
+                            chosenFrom === "choose" ||
+                            chosenTo === "choose" ||
+                            amountFrom === 0 ||
+                            exchangeRate === 0
+                        }
                     >
                         Swap
                     </Button>
+                    {exchangeRate === 0 ? (
+                        <Alert variant="warning">
+                            One or more of the liquidity pools is empty or
+                            unselected.
+                        </Alert>
+                    ) : null}
                 </Card.Body>
             </Card>
         </div>
