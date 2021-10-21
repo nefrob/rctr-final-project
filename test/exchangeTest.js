449136b47fb4ae171519ef5f4a1e28d6f2ff14e3
nefrob <nefrob.skyguy@gmail.com>
2021-10-21 15:45:30 -0600

    Update exchange tests

--- test/exchangeTest.js
+++ test/exchangeTest.js
@@ -1,17 +1,17 @@
-var assert = require("assert");
+const assert = require("assert");
+const truffleAssert = require("truffle-assertions");
 
 // Utils
-
-const getEthBalance = async (address) => await web3.eth.getBalance(address);
-
-const getTokBalance = async (token, address) => await token.balanceOf(address);
 
 const ethToWei = (eth) => web3.utils.toWei(eth.toString());
 
 const weiToEth = (wei) => web3.utils.fromWei(wei);
 
-const getGasCost = async (tx) =>
-    (await web3.eth.getGasPrice()) * tx.receipt.gasUsed;
+const getEthBalance = async (address) =>
+    weiToEth(await web3.eth.getBalance(address));
+
+const getTokBalance = async (token, address) =>
+    weiToEth(await token.balanceOf(address));
 
 // Tests
 
@@ -38,7 +38,7 @@ contract("Exchange", (accounts) => {
         );
     });
 
-    it("Correct setup", async () => {
+    it("Correct construction", async () => {
         const name = await exchange.name();
         assert.equal(name, "Sandman Swap");
 
@@ -53,74 +53,75 @@ contract("Exchange", (accounts) => {
 
     describe("Empty liquidity pool handle errors", async () => {
         it("Error add no liquidity", async () => {
-            try {
-                await exchange.addLiquidity(0, { value: 0 });
-            } catch (err) {
-                assert.ok(err.message);
-            }
+            await truffleAssert.reverts(
+                exchange.addLiquidity(0, { value: 0 }),
+                "addLiquidity: deposit amount too small"
+            );
         });
 
         it("Error remove liquidity", async () => {
-            try {
-                await exchange.removeLiquidity(ethToWei(10));
-            } catch (err) {
-                assert.ok(err.message);
-            }
+            await truffleAssert.reverts(
+                exchange.removeLiquidity(10),
+                "removeLiquidity: not enough liquidity"
+            );
         });
     });
 
     describe("Add liquidity", async () => {
         beforeEach("Add liquidity setup", async () => {
-            // tok = 20, eth = 10, lpMint = ethReserve = 10
-            assert.ok(
+            // tok = 20, eth = 10, lp = ethReserve = 10
+            truffleAssert.passes(
                 await sampleToken1.approve(exchange.address, ethToWei(20))
             );
             await exchange.addLiquidity(ethToWei(20), { value: ethToWei(10) });
         });
 
         it("Add liquidity empty pool", async () => {
-            const lpAmount = weiToEth(
-                await getTokBalance(exchange, accounts[0])
-            );
+            const lpAmount = await getTokBalance(exchange, accounts[0]);
             assert.equal(weiToEth(await exchange.totalSupply()), lpAmount);
             assert.equal(lpAmount, 10);
 
-            const tokenReserves = weiToEth(
-                await getTokBalance(sampleToken1, exchange.address)
+            const tokenReserves = await getTokBalance(
+                sampleToken1,
+                exchange.address
             );
             assert.equal(tokenReserves, 20);
 
-            const ethReserves = weiToEth(await getEthBalance(exchange.address));
+            const ethReserves = await getEthBalance(exchange.address);
             assert.equal(ethReserves, 10);
         });
 
         it("Add liquidity non-empty pool", async () => {
+            // tok = 20, eth = 10, lpSupply = 10
             // lpMint = ethDeposited / ethPool * lpSupply = 5 / 10 * 10 = 5
-            assert.ok(
+
+            truffleAssert.passes(
                 await sampleToken1.approve(exchange.address, ethToWei(10))
             );
-            await exchange.addLiquidity(ethToWei(10), { value: ethToWei(5) });
 
-            const lpAmount = weiToEth(
-                await getTokBalance(exchange, accounts[0])
-            );
+            await exchange.addLiquidity(ethToWei(10), {
+                value: ethToWei(5),
+            });
+
+            const lpAmount = await getTokBalance(exchange, accounts[0]);
             assert.equal(weiToEth(await exchange.totalSupply()), lpAmount);
             assert.equal(lpAmount, 15);
 
-            const tokenReserves = weiToEth(
-                await getTokBalance(sampleToken1, exchange.address)
+            const tokenReserves = await getTokBalance(
+                sampleToken1,
+                exchange.address
             );
             assert.equal(tokenReserves, 30);
 
-            const ethReserves = weiToEth(await getEthBalance(exchange.address));
+            const ethReserves = await getEthBalance(exchange.address);
             assert.equal(ethReserves, 15);
         });
     });
 
     describe("Remove liquidity non-empty pool", async () => {
         beforeEach("Remove liquidity setup", async () => {
-            // tok = 20, eth = 10, lpMint = ethReserve = 10
-            assert.ok(
+            // tok = 20, eth = 10, lp = ethReserve = 10
+            truffleAssert.passes(
                 await sampleToken1.approve(exchange.address, ethToWei(20))
             );
             await exchange.addLiquidity(ethToWei(20), {
@@ -131,18 +132,17 @@ contract("Exchange", (accounts) => {
         it("Remove liquidity", async () => {
             await exchange.removeLiquidity(ethToWei(5));
 
-            const lpAmount = weiToEth(
-                await getTokBalance(exchange, accounts[0])
-            );
+            const lpAmount = await getTokBalance(exchange, accounts[0]);
             assert.equal(weiToEth(await exchange.totalSupply()), lpAmount);
             assert.equal(lpAmount, 5);
 
-            const tokenReserves = weiToEth(
-                await getTokBalance(sampleToken1, exchange.address)
+            const tokenReserves = await getTokBalance(
+                sampleToken1,
+                exchange.address
             );
             assert.equal(tokenReserves, 10);
 
-            const ethReserves = weiToEth(await getEthBalance(exchange.address));
+            const ethReserves = await getEthBalance(exchange.address);
             assert.equal(ethReserves, 5);
         });
 
@@ -153,54 +153,50 @@ contract("Exchange", (accounts) => {
 
             await exchange.removeLiquidity(ethToWei(10));
 
-            const lpAmount = weiToEth(
-                await getTokBalance(exchange, accounts[0])
-            );
+            const lpAmount = await getTokBalance(exchange, accounts[0]);
             assert.equal(weiToEth(await exchange.totalSupply()), lpAmount);
             assert.equal(lpAmount, 0);
 
-            const tokenReserves = weiToEth(
-                await getTokBalance(sampleToken1, exchange.address)
+            const tokenReserves = await getTokBalance(
+                sampleToken1,
+                exchange.address
             );
             assert.equal(tokenReserves, 0);
 
-            const ethReserves = weiToEth(await getEthBalance(exchange.address));
+            const ethReserves = await getEthBalance(exchange.address);
             assert.equal(ethReserves, 0);
         });
 
         it("Error remove no liquidity", async () => {
-            try {
-                await exchange.removeLiquidity(0);
-            } catch (err) {
-                assert.ok(err.message);
-            }
+            await truffleAssert.reverts(
+                exchange.removeLiquidity(0),
+                "removeLiquidity: lpAmount too small"
+            );
         });
 
         it("Error remove more than liquidity", async () => {
-            try {
-                await exchange.removeLiquidity(ethToWei(15));
-            } catch (err) {
-                assert.ok(err.message);
-            }
+            await truffleAssert.reverts(
+                exchange.removeLiquidity(ethToWei(15)),
+                "removeLiquidity: not enough liquidity"
+            );
         });
     });
 
     describe("ETH to Token Swap", async () => {
         describe("Empty liquidity pool", async () => {
             it("Error try to swap", async () => {
-                try {
-                    await exchange.ethToTokenExchange(ethToWei(500), {
+                await truffleAssert.reverts(
+                    exchange.ethToTokenExchange(ethToWei(500), {
                         value: ethToWei(10),
-                    });
-                } catch (err) {
-                    assert.ok(err.message);
-                }
+                    }),
+                    "getExchangePrice: invalid reserve amounts"
+                );
             });
         });
 
         describe("Non-empty liquidity pool", async () => {
             beforeEach("Setup", async () => {
-                assert.ok(
+                truffleAssert.passes(
                     await sampleToken1.approve(exchange.address, ethToWei(500))
                 );
                 await exchange.addLiquidity(ethToWei(500), {
@@ -209,21 +205,19 @@ contract("Exchange", (accounts) => {
             });
 
             it("Error invalid Token amount", async () => {
-                try {
-                    await exchange.ethToTokenExchange(0);
-                } catch (err) {
-                    assert.ok(err.message);
-                }
+                await truffleAssert.reverts(
+                    exchange.ethToTokenExchange(0),
+                    "ethToTokenTransfer: desiredTokenAmount too small"
+                );
             });
 
             it("Error not enough ETH for desired Token amount", async () => {
-                try {
-                    await exchange.ethToTokenExchange(ethToWei(500), {
+                await truffleAssert.reverts(
+                    exchange.ethToTokenExchange(ethToWei(500), {
                         value: ethToWei(1),
-                    });
-                } catch (err) {
-                    assert.ok(err.message);
-                }
+                    }),
+                    "ethToTokenExchange: not enough tokens"
+                );
             });
 
             it("Swap correct Token amount", async () => {
@@ -243,13 +237,10 @@ contract("Exchange", (accounts) => {
 
                 assert.equal(
                     await getTokBalance(sampleToken1, exchange.address),
-                    ethToWei(500) - (userTokAfter - userTokBefore)
+                    500 - (userTokAfter - userTokBefore)
                 );
 
-                assert.equal(
-                    await getEthBalance(exchange.address),
-                    ethToWei(11)
-                );
+                assert.equal(await getEthBalance(exchange.address), 11);
             });
         });
     });
@@ -257,20 +248,16 @@ contract("Exchange", (accounts) => {
     describe("Token to ETH Swap", async () => {
         describe("Empty liquidity pool", async () => {
             it("Error try to swap", async () => {
-                try {
-                    await exchange.tokenToEthExchange(
-                        ethToWei(10),
-                        ethToWei(10)
-                    );
-                } catch (err) {
-                    assert.ok(err.message);
-                }
+                await truffleAssert.reverts(
+                    exchange.tokenToEthExchange(ethToWei(10), ethToWei(10)),
+                    "getExchangePrice: invalid reserve amounts"
+                );
             });
         });
 
         describe("Non-empty liquidity pool", async () => {
             beforeEach("Setup", async () => {
-                assert.ok(
+                truffleAssert.passes(
                     await sampleToken1.approve(exchange.address, ethToWei(500))
                 );
                 await exchange.addLiquidity(ethToWei(500), {
@@ -279,28 +266,22 @@ contract("Exchange", (accounts) => {
             });
 
             it("Error invalid Token/ETH amount", async () => {
-                try {
-                    await exchange.tokenToEthExchange(0, 0);
-                } catch (err) {
-                    assert.ok(err.message);
-                }
+                await truffleAssert.reverts(
+                    exchange.tokenToEthExchange(0, 0),
+                    "tokenToEthExchange: exchange amount too small"
+                );
             });
 
             it("Error not enough Token for desired ETH amount", async () => {
-                try {
-                    await sampleToken1.approve(exchange.address, ethToWei(1));
-                    await exchange.tokenToEthExchange(ethToWei(1), ethToWei(1));
-                } catch (err) {
-                    assert.ok(err.message);
-                }
+                sampleToken1.approve(exchange.address, ethToWei(1));
+
+                await truffleAssert.reverts(
+                    exchange.tokenToEthExchange(ethToWei(1), ethToWei(1)),
+                    "tokenToEthExchange: not enough eth"
+                );
             });
 
             it("Swap correct ETH amount", async () => {
-                const userTokBefore = await getTokBalance(
-                    sampleToken1,
-                    accounts[0]
-                );
-
                 await sampleToken1.approve(
                     exchange.address,
                     ethToWei("55.722723726735762845")
@@ -310,25 +291,12 @@ contract("Exchange", (accounts) => {
                     ethToWei(1)
                 );
 
-                const userTokAfter = await getTokBalance(
-                    sampleToken1,
-                    accounts[0]
-                );
-
                 assert.equal(
                     await getTokBalance(sampleToken1, exchange.address),
-                    ethToWei("555.722723726735762845")
+                    "555.722723726735762845"
                 );
 
-                // assert.equal(
-                //     userTokBefore - userTokAfter, // fixme: precision issue need to fix
-                //     ethToWei("55.722723726735762845")
-                // );
-
-                assert.equal(
-                    await getEthBalance(exchange.address),
-                    ethToWei(9)
-                );
+                assert.equal(await getEthBalance(exchange.address), 9);
             });
         });
     });
@@ -347,45 +315,27 @@ contract("Exchange", (accounts) => {
 
         describe("Parameter setup checks", async () => {
             it("Error invalid token amounts", async () => {
-                try {
-                    await exchange.tokenToTokenExchange(
-                        0,
-                        0,
-                        sampleToken2.address
-                    );
-                } catch (err) {
-                    assert.ok(err.message);
-                }
+                await truffleAssert.reverts(
+                    exchange.tokenToTokenExchange(0, 0, sampleToken2.address),
+                    "tokenToTokenExchange: token amounts too small"
+                );
             });
 
             it("Error invalid Token2 address", async () => {
-                try {
-                    await exchange.tokenToTokenExchange(1, 1, 0x0);
-                } catch (err) {
-                    assert.ok(err.message);
-                }
-
-                try {
-                    await exchange.tokenToTokenExchange(
-                        1,
-                        1,
-                        sampleToken1.address
-                    );
-                } catch (err) {
-                    assert.ok(err.message);
-                }
+                await truffleAssert.fails(
+                    exchange.tokenToTokenExchange(1, 1, 0x0)
+                );
+                await truffleAssert.reverts(
+                    exchange.tokenToTokenExchange(1, 1, sampleToken1.address),
+                    "tokenToTokenExchange: invalid otherTokenAddress"
+                );
             });
 
             it("Error non-existant Token2 exchange address", async () => {
-                try {
-                    await exchange.tokenToTokenExchange(
-                        1,
-                        1,
-                        sampleToken2.address
-                    );
-                } catch (err) {
-                    assert.ok(err.message);
-                }
+                await truffleAssert.reverts(
+                    exchange.tokenToTokenExchange(1, 1, sampleToken2.address),
+                    "tokenToTokenExchange: otherTokenAddress does not have not an exchange"
+                );
             });
         });
 
@@ -399,15 +349,14 @@ contract("Exchange", (accounts) => {
 
             describe("Empty liquidity pool", async () => {
                 it("Error try to swap", async () => {
-                    try {
-                        await exchange.tokenToTokenExchange(
+                    await truffleAssert.reverts(
+                        exchange.tokenToTokenExchange(
                             ethToWei(10),
                             ethToWei(10),
                             sampleToken2.address
-                        );
-                    } catch (err) {
-                        assert.ok(err.message);
-                    }
+                        ),
+                        "getExchangePrice: invalid reserve amounts"
+                    );
                 });
             });
 
@@ -428,19 +377,16 @@ contract("Exchange", (accounts) => {
                 });
 
                 it("Error not enough Token1 for desired Token2 amount", async () => {
-                    try {
-                        await sampleToken1.approve(
-                            exchange.address,
-                            ethToWei(1)
-                        );
-                        await exchange.tokenToTokenExchange(
+                    await sampleToken1.approve(exchange.address, ethToWei(1));
+
+                    await truffleAssert.reverts(
+                        exchange.tokenToTokenExchange(
                             ethToWei(1),
                             ethToWei(100),
                             sampleToken2.address
-                        );
-                    } catch (err) {
-                        assert.ok(err.message);
-                    }
+                        ),
+                        "ethToTokenExchange: not enough tokens"
+                    );
                 });
 
                 it("Swap correct Token2 amount", async () => {
@@ -459,10 +405,7 @@ contract("Exchange", (accounts) => {
                         accounts[0]
                     );
 
-                    assert.equal(
-                        userTok2After,
-                        ethToWei("90.661089388014913158")
-                    );
+                    assert.equal(userTok2After, "90.661089388014913158");
                 });
             });
         });
@@ -470,7 +413,7 @@ contract("Exchange", (accounts) => {
 
     describe("Exchange rates", async () => {
         beforeEach("Setup", async () => {
-            assert.ok(
+            truffleAssert.passes(
                 await sampleToken1.approve(exchange.address, ethToWei(500))
             );
             await exchange.addLiquidity(ethToWei(500), {
